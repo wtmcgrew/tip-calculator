0b6e64a3347da0567b98cf6ba1d0e2485e6dbf62
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-29 20:38:33 -0500

    Conversion of totalBill to Number to calculate bill total

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
 const amount = document.querySelector("#amount");
-const guests = document.querySelector("#guests");
 const tipPercentage = document.querySelector("#tip");
 const tipAmount = document.querySelector("#tip-amount");
 const totalAmount = document.querySelector("#total-amount");
 
+// Calculation of bill total and resetting empty values for variables
 calculateBillPlusTip = () => {
-	const totalBill = ((amount.value * tipPercentage.value) + (amount.value) / (guests.value)).toFixed(2);
+	const totalBill = Number(((amount.value * tipPercentage.value)) + Number((amount.value))).toFixed(2);
 	amount.value = "";
-	guests.value = "";
 	tipPercentage.value = "";
 
 	if(totalBill === "NaN") {
-		totalAmount.textContent = "$0 due"
+		totalAmount.textContent = "$0.00 due";
 		showTotalAmount();
 	}
 	else {
-		totalAmount.textContent = "Bill Total $ " + totalBill;
+		totalAmount.textContent = "Bill Total $" + totalBill;
 		showTotalAmount();
 	}
-
 }
 
+// Calculation of bill total and resetting empty values for variables
 calculateTip = () => {
-	const tip = ((amount.value * tipPercentage.value) / (guests.value)).toFixed(2);
+	const tip = (amount.value * tipPercentage.value).toFixed(2);
 	amount.value = "";
-	guests.value = "";
 	tipPercentage.value = "";
 
 	if(tip === "NaN") {
-		tipAmount.textContent = "Tip $0 each";
+		tipAmount.textContent = "Tip $0.00";
 		showTipAmount();
 	}
 	else {
-		tipAmount.textContent = "Tip $" + tip + " each";
+		tipAmount.textContent = "Tip $" + tip;
 		showTipAmount();
 	}
 }
 
+// 
 showTipAmount = () => {
 	let showTip = document.querySelector("#tip-amount");
   	// Add the "show-tip" class to DIV
 	showTip.className = "show-tip";
-	setTimeout(function(){ showTip.className = showTip.className.replace("show-tip", ""); }, 3000);
+	setTimeout(function(){ showTip.className = showTip.className.replace("show-tip", ""); }, 3000); // Will show for 3 seconds using setTimeout()
 }
 
 showTotalAmount = () => {
 	let showTotal = document.querySelector("#total-amount");
   	// Add the "show-total" class to DIV
 	showTotal.className = "show-total";
-	setTimeout(function(){ showTotal.className = showTotal.className.replace("show-total", ""); }, 3000);
+	setTimeout(function(){ showTotal.className = showTotal.className.replace("show-total", ""); }, 3000); // Will show for 3 seconds using setTimeout()
 }
